090d65a1ab33425ecbcea62b0776bc761e644500
Thibaut <khelil.thibaut@orange.fr>
2019-07-02 10:32:11 +0200

    Ajout du projet

---
+++ app.js
@@ -0,0 +1,68 @@
+// IMPORT
+let express = require('express')
+let app = express()
+
+
+// TEMPLATE ENGINE EJS
+app.set('view engine', 'ejs')
+
+// STATIC FILES
+app.use('/assets', express.static('public'))
+app.use('/assets', express.static('images'))
+
+
+// ROUTING
+
+
+// LANDING PAGE
+app.get('/accueil', (request, response) => {
+
+	console.log('Page d\'acceuil')
+
+	response.render('layouts/home/accueil')
+
+})
+
+// LEARN OTAMATONE
+app.get('/apprendre', (request, response) => {
+
+	console.log('Apprendre l\'otamatone')
+
+	response.render('layouts/learn/apprendre')
+
+})
+
+// NEWS PAGE
+app.get('/news', (request, response) => {
+
+	console.log('Page de news')
+
+})
+
+// SHOP 
+app.get('/shop', (request, response) => {
+
+	console.log('Page shopping')
+
+	response.render('layouts/shop/shopping')
+
+})
+
+// VIDEO PAGE
+app.get('/videos', (request, response) => {
+
+	console.log('Page vidéos')
+
+})
+
+// CONTACT PAGE
+app.get('/contact', (request, response) => {
+
+	console.log('Page de contact')
+
+})
+
+
+// LISTEN PORT
+
+app.listen(8080)
